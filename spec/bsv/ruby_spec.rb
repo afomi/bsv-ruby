8b709d8add38b2293b8ce47f3845a0c0ba319f2a
Ryan Wold <wold@afomi.com>
2020-02-22 21:36:44 -0800

    bundle gem bsv-ruby

---
+++ spec/bsv/ruby_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Bsv::Ruby do
+  it "has a version number" do
+    expect(Bsv::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
